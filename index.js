3181bcb9ff5fa0bd94ecca1e196e3aa042517466
12050114090@students.uin-suska.ac.id <12050114090@students.uin-suska.ac.id>
2022-07-01 23:45:11 +0700

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 
+const port = process.env.PORT || 8000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(8000, ()=> console.log('Server Running...'));
+app.listen(port, ()=> console.log('Server Running...'));
